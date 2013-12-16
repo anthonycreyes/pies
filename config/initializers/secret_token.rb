2e285b6f3e416f4e23f628f9aff4ad39df6e962d
Anthony Reyes <areyes2k@gmail.com>
2013-12-15 20:53:46 -0800

    initial commit of OneMonthRails_new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pies::Application.config.secret_key_base = '32e3d39059a0bc02b021aa1a3859d06be8d08c77d4e951f7ec869d2cf61b204dda95e4d0f4468308eb28936f73844a504c8d4cf471ea48f496639fc15e628d1a'
