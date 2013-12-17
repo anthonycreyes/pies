48dbe638397c8ebcee45dac0c632461338f91bb7
Anthony Reyes <areyes2k@gmail.com>
2013-12-16 23:59:15 -0800

    Added Bootstrap Gem + new page from Treehouse

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pies::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "strap" => "pages#strap"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
